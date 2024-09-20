48770b3caf6cd45c60acbd044f17ed8e66ec1021
thataruby <thata@dfi.id>
2024-09-20 21:24:44 +0700

    failed test

--- main/tests.py
+++ main/tests.py
@@ -24,3 +24,8 @@ class MainTest(TestCase):
           mood_intensity = 8,
         )
         self.assertTrue(mood.is_mood_strong)
+
+    def test_main_template_uses_correct_page_titl(self):
+        response = Client().get("/")
+        html_response = response.content.decode("utf8")
+        self.assertIn("PBD Mental Health Tracker", html_response)
