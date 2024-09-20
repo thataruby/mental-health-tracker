40ea1bf0e0741773a63cf4763d424c5fe48a72f5
thataruby <thata@dfi.id>
2024-09-20 21:36:14 +0700

    failed test

--- main/tests.py
+++ main/tests.py
@@ -18,14 +18,14 @@ class MainTest(TestCase):
     def test_strong_mood_user(self):
         now = timezone.now()
         mood = MoodEntry.objects.create(
-          mood="Happy",
-          time = now,
-          feelings = "I'm happy, even though my clothes are soaked from the rain :(",
-          mood_intensity = 8,
+            mood="Happy",
+            time = now,
+            feelings = "I'm happy, even though my clothes are soaked from the rain :(",
+            mood_intensity = 8,
         )
         self.assertTrue(mood.is_mood_strong)
 
-    def test_main_template_uses_correct_page_titl(self):
+    def test_main_template_uses_correct_page_title(self):
         response = Client().get("/")
         html_response = response.content.decode("utf8")
         self.assertIn("PBD Mental Health Tracker", html_response)
